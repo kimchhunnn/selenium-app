d423adeb743b1f5ef9e98750675094a85c03ea09
kimchhunnn <76899805+kimchhunnn@users.noreply.github.com>
2021-10-28 11:43:03 +0700

    feat: test script for Selenium Grid

--- src/com/storybook_app/login/Page_Login.java
+++ src/com/storybook_app/login/Page_Login.java
@@ -1,44 +1,51 @@
 package com.storybook_app.login;
 
 import org.testng.annotations.*;
+
+import java.net.URL;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 
 public class Page_Login {
-	public String baseUrl = "http://localhost:8080/login/";
+	public String baseUrl = "http://172.20.10.6:8080/login/";
+	public String URL = "http://localhost:4444/wd/hub";
 	public WebDriver webDriver;
+	public DesiredCapabilities caps = new DesiredCapabilities();
     
 	@BeforeTest
 	@Parameters("browser")
 	public void beforeTest(String browser) throws Exception {
+		caps.setPlatform(Platform.LINUX);
 		switch(browser.toLowerCase()) {
 			case "chrome":
-				System.out.println("ChromeDriver");
-				webDriver = new ChromeDriver();
+				System.out.println("Set Browser to Chrome");
+				caps.setBrowserName("chrome");
 				break;
-			case "safari":
-				System.out.println("SafariDriver");
-				webDriver = new SafariDriver();
+			case "firefox":
+				System.out.println("Set Browser to Firefox");
+				caps.setBrowserName("firefox");
 			    break;
 			default:
 				System.out.println("Incorrect Browser");
 			  	throw new Exception("Incorrect Browser");
 		};
+        webDriver = new RemoteWebDriver(new URL(URL), caps);
 	}
 
 	@AfterTest
 	public void afterTest() {
+		System.out.println("Close Browser And Quit Test");
 		webDriver.close();
 		webDriver.quit();
 	}
   
 	@Test
 	public void Should_RedirecToHomePage_WhenLoginInfoValid() {
-		
 		String storeId = "00001";
 		String password = "00001";
 		String expectedHomeHeader = "Welcome to Storybook App";
@@ -62,7 +69,6 @@ public class Page_Login {
 	
 	@Test
 	public void Should_FailedToLogin_WhenLoginInfoInvalid() {
-		
 		String storeId = "00002";
 		String password = "00002";
 		String expectedLoginHeader = "管理者ログイン";
@@ -86,7 +92,6 @@ public class Page_Login {
 	
 	@Test
 	public void Should_RedirectToForgetPasswordPage_WhenClickForgetPasswordBtn() {
-
 		String expectedForgetPasswordHeader = "Reset Your Password Here";
 		
 		webDriver.get(baseUrl);
--- src/com/storybook_app/login/Page_Login_UI.java
+++ src/com/storybook_app/login/Page_Login_UI.java
@@ -1,38 +1,46 @@
 package com.storybook_app.login;
 
 import org.testng.annotations.*;
+
+import java.net.URL;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 
 public class Page_Login_UI {
-	public String baseUrl = "http://localhost:8080/login/";
-	public WebDriver webDriver;
+	public String baseUrl = "http://172.20.10.6:8080/login/";
+	public String URL = "http://localhost:4444/wd/hub";
+	public static WebDriver webDriver;
+	public DesiredCapabilities caps = new DesiredCapabilities();
     
 	@BeforeTest
 	@Parameters("browser")
 	public void beforeTest(String browser) throws Exception {
+		caps.setPlatform(Platform.LINUX);
 		switch(browser.toLowerCase()) {
 			case "chrome":
-				System.out.println("ChromeDriver");
-				webDriver = new ChromeDriver();
+				System.out.println("Set Browser to Chrome");
+				caps.setBrowserName("chrome");
 				break;
-			case "safari":
-				System.out.println("SafariDriver");
-				webDriver = new SafariDriver();
+			case "firefox":
+				System.out.println("Set Browser to Firefox");
+				caps.setBrowserName("firefox");
 			    break;
 			default:
 				System.out.println("Incorrect Browser");
 			  	throw new Exception("Incorrect Browser");
 		};
-		    // code block
+        webDriver = new RemoteWebDriver(new URL(URL), caps);
 	}
 
 	@AfterTest
 	public void afterTest() {
+		System.out.println("Close Browser And Quit Test");
 		webDriver.close();
 		webDriver.quit();
 	}
